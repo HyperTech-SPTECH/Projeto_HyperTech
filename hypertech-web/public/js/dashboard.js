368f7f9e2e68c78c5a0c3142478e2525b031642f
Eduardo-xX <eduardo.nascimento@sptech.school>
2026-04-18 21:55:37 -0300

    Subindo Tela base da Dashboard e alguns ajustes no Site...

---
+++ hypertech-web/public/js/dashboard.js
@@ -0,0 +1,26 @@
+function alterarTela(ev) {
+    let evAtual = ev.target
+    let classesAtuais = evAtual.className
+    if (!classesAtuais.includes("optionNav")) {
+        evAtual = evAtual.parentElement
+    }
+
+    // Trocando o selecionado do NavBar
+    let navBar = document.getElementById('idNavLeftBar')
+    for (let i = 0; i < navBar.children.length; i++) {
+        navBar.children[i].classList.remove('optionSelecioned')
+    }
+    document.getElementById(evAtual.id).classList.add('optionSelecioned')
+
+    // Trocando a Tela que está aparecendo
+    let telas = document.getElementById('idConteudo')
+    for (let i = 0; i < telas.children.length; i++) {
+        telas.children[i].style.display = 'none'
+    }
+    let telaCerta = "idTela" + evAtual.id.split('idOption')[1]
+    document.getElementById(telaCerta).style.display = 'block'
+}
+
+document.getElementById('idOptionDashboard').addEventListener('click', alterarTela)
+document.getElementById('idOptionRotas').addEventListener('click', alterarTela)
+document.getElementById('idOptionCadFunc').addEventListener('click', alterarTela)
